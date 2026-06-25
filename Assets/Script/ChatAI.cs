90783edb9761b6ba6af77a3d7fdf58b455750d67
Sheikh Samiur Rahman <samiur.university@gmail.com>
2026-06-25 19:54:10 +0600

    1

---
+++ Assets/Script/ChatAI.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ChatAI : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
